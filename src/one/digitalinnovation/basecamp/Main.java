77da268df52d12dce2fb997d0cea05d015479c88
Nathally Nunes <nathally.nunes@gmail.com>
2022-05-06 17:10:49 -0300

    Exercicio Sobrecarga

---
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //Quadrilátero
+        System.out.println("Exercicio Quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(5,8, 9);
+        Quadrilatero.area(5f,5f);
+
+
+    }
+}
